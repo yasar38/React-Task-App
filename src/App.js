447439e02737cc544f73f1f6394a36119c0413af
Yaşar İsmet Yılmaz <yasar3814@gmail.com>
2023-04-09 19:09:46 +0300

    Json eklendi.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,35 @@ import TaskList from './Components/TaskList';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const createTask = (title, taskDec) => {
+  const createTask = async (title, taskDec) => {
+    const response = await axios.post('http://localhost:3001/tasks', {
+      title, taskDec
+    });
     const createdTask = [
-      ...tasks, {
-        id: Math.round(Math.random() * 999999),
-        title, //title: title => bununla aynı anlama geliyor isimler aynıtsa
-        taskDec
-      }
+      ...tasks, response.data
+      // {
+      //    id: Math.round(Math.random() * 999999),
+      //    title, //title: title => bununla aynı anlama geliyor isimler aynıtsa
+      //    taskDec
+      // }
     ];
     setTasks(createdTask);
   }
-  const deleteTaskById = (id) => {
+  const fetchTasks = async () => {
+    const response = await axios.get('http://localhost:3001/tasks');
+    setTasks(response.data)
+  }
+  useEffect(() => {
+    fetchTasks()
+  }, [])
+
+  const deleteTaskById = async (id) => {
+    const response = await axios.delete('http://localhost:3001/tasks/${id}');
     const afterDeletingTasks = tasks.filter((task) => {
       return task.id !== id;
     })
